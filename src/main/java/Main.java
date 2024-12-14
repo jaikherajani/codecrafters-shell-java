084c8daac3fcddfcabd583e62616497269b50d3f
Jai Kherajani <jai.kherajani19@gmail.com>
2024-12-15 02:03:28 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -20,7 +22,20 @@ public class Main {
             String[] ops = input.split(" ", 2);
 
             if (SupportedCommands.findByValue(ops[0]) == null) {
-                System.out.println(input + ": not found");
+                String path = getPath(ops[0]);
+                if (path != null) {
+                    //System.out.println(ops[1] + " is " + path);
+                    ProcessBuilder pb = new ProcessBuilder(path, ops[1]);
+                    Process p = pb.start();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        System.out.println(line);
+                    }
+                }
+                else
+                    System.out.println(ops[0] + ": not found");
             }
             else {
                 SupportedCommands command = SupportedCommands.findByValue(ops[0]);
